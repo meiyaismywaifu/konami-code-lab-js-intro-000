ee6ab96a97cab7695defe4679ad6bb3af24620a4
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-20 23:53:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init() { // why wrap inside a function?
+  var konamiCounter = 0;
+
+  // event listener to document.body
+  document.body.addEventListener("keydown", (event) => {
+    var key = event.key;
+    if (key === codes[konamiCounter]){
+      konamiCounter++;
+    } else {
+      konamiCounter = 0;
+    }
+
+    if (konamiCounter === 10){
+      konamiCounter = 0;
+      alert("Hurray");
+    }
+  })
+  // ...this was simple.
+  // maybe wouldn't have been if i wasn't told "start with (line 18)".
 }
